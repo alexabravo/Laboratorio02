08eb675801ec1fcab8e6ac1f9dfc41023d1433a6
alexabravo <bravo.alexa25@gmail.com>
2021-02-17 00:08:39 -0600

    Listoooo! (Funciona nada mas que se borran los numeros al presionar cualquier operador, pero al dar igual si se realiza la operacion)

--- app/src/main/java/com/example/laboratorio2/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio2/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(){
 
+    private var num1 = 0.0
+    private var num2 = 0.0
+    private var operacion = 0
+
     private lateinit var txtResult: TextView
     private lateinit var btnOne: Button
     private lateinit var btnTwo: Button
@@ -18,11 +22,25 @@ class MainActivity : AppCompatActivity(){
     private lateinit var btnEight: Button
     private lateinit var btnNine: Button
     private lateinit var btnCero: Button
+    private lateinit var btnPoint: Button
+
     private lateinit var btnSum: Button
+    private lateinit var btnRes: Button
+    private lateinit var btnMul: Button
+    private lateinit var btnDiv: Button
+    private lateinit var btnCle: Button
+    private lateinit var btnBor: Button
+    private lateinit var btnIgl: Button
+
+    private var operador: String? = null
+    private var valorAnterior: String ? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var resultado: Float = 0f
 
         txtResult = findViewById(R.id.Etiqueta)
         btnOne = findViewById(R.id.Uno)
@@ -35,52 +53,87 @@ class MainActivity : AppCompatActivity(){
         btnEight = findViewById(R.id.Ocho)
         btnNine = findViewById(R.id.Nueve)
         btnCero = findViewById(R.id.Cero)
+        btnPoint = findViewById(R.id.Punto)
         btnSum = findViewById(R.id.Suma)
+        btnRes = findViewById(R.id.Resta)
+        btnMul = findViewById(R.id.Multiplica)
+        btnDiv = findViewById(R.id.Divide)
+        btnCle = findViewById(R.id.Clean)
+        btnBor = findViewById(R.id.Borrar)
+        btnIgl = findViewById(R.id.Igual)
 
-        btnCero.setOnClickListener {
-            txtResult.append(getString(R.string.cero))
+        txtResult.text = "0"
+        operacion = SIN_OPERACION
+
+        btnOne.setOnClickListener { numberPressed("1") }
+        btnTwo.setOnClickListener { numberPressed("2") }
+        btnThree.setOnClickListener { numberPressed("3") }
+        btnFour.setOnClickListener { numberPressed("4") }
+        btnFive.setOnClickListener { numberPressed("5") }
+        btnSix.setOnClickListener { numberPressed("6") }
+        btnSeven.setOnClickListener { numberPressed("7") }
+        btnEight.setOnClickListener { numberPressed("8") }
+        btnNine.setOnClickListener { numberPressed("9") }
+        btnCero.setOnClickListener { numberPressed("0") }
+        btnPoint.setOnClickListener { numberPressed(".") }
+
+        btnCle.setOnClickListener { resetAll() }
+
+        btnSum.setOnClickListener { operationPressed(SUMA) }
+        btnRes.setOnClickListener { operationPressed(RESTA) }
+        btnMul.setOnClickListener { operationPressed(MULTIPLICACION) }
+        btnDiv.setOnClickListener { operationPressed(DIVISION) }
+
+        btnIgl.setOnClickListener { resolvePressed() }
+    }
+
+    private fun numberPressed(num: String){
+        if(txtResult.text == "0" && num != ".") {
+            txtResult.text = "$num"
+        } else {
+            txtResult.text = "${txtResult.text}$num"
         }
 
-        btnOne.setOnClickListener {
-            txtResult.append(getString(R.string.one))
-        }
-
-        btnTwo.setOnClickListener {
-            txtResult.append(getString(R.string.two))
-        }
-
-        btnThree.setOnClickListener {
-            txtResult.append(getString(R.string.three))
-        }
-
-        btnFour.setOnClickListener {
-            txtResult.append(getString(R.string.four))
-        }
-
-        btnFive.setOnClickListener {
-            txtResult.append(getString(R.string.five))
-        }
-
-        btnSix.setOnClickListener {
-            txtResult.append(getString(R.string.six))
-        }
-
-        btnSeven.setOnClickListener {
-            txtResult.append(getString(R.string.seven))
-        }
-
-        btnEight.setOnClickListener {
-            txtResult.append(getString(R.string.eight))
-        }
-
-        btnNine.setOnClickListener {
-            txtResult.append(getString(R.string.nine))
-        }
-
-        btnSum.setOnClickListener {
-            val valueA = txtResult.text.toString().toDouble()
-            txtResult.text = ""
+        if(operacion == SIN_OPERACION){
+            num1 = txtResult.text.toString().toDouble()
+        } else {
+            num2 = txtResult.text.toString().toDouble()
         }
     }
-}
 
+    private fun operationPressed(operacion: Int){
+        this.operacion = operacion
+        num1 = txtResult.text.toString().toDouble()
+
+        txtResult.text = "0"
+    }
+
+    private fun resolvePressed(){
+
+        val result = when(operacion) {
+            SUMA -> num1 + num2
+            RESTA -> num1 - num2
+            MULTIPLICACION -> num1 * num2
+            DIVISION -> num1 / num2
+            else -> 0
+        }
+
+        num1 = result as Double
+
+        txtResult.text = if("$result".endsWith(".0")) { "$result".replace(".0","") } else { "%.2f".format(result) }
+    }
+
+    private fun resetAll(){
+        txtResult.text = "0"
+        num1 = 0.0
+        num2 = 0.0
+    }
+
+    companion object {
+        const val SUMA = 1
+        const val RESTA = 2
+        const val MULTIPLICACION = 3
+        const val DIVISION = 4
+        const val SIN_OPERACION = 0
+    }
+}
